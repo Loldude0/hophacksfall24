5592de05c066a7f943d828551b33c78b2cbb167e
Abhyuday Goyal <abhyuday2106@gmail.com>
2024-09-14 04:11:38 -0400

    added sms

--- main_server.py
+++ main_server.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import gridfs
 from server_ai_pipeline import diagnose_patient
 from datetime import datetime
+from send_sms import send_sms
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
@@ -34,6 +35,7 @@ def post_basic_info():
     else:
         return jsonify({"status": "error", "message": "User already exists"})
 
+
 @app.route("/get_activity_info", methods=["GET"])
 def get_activity_info():
     user_id = request.args.get("user_id")
@@ -41,7 +43,7 @@ def get_activity_info():
     if activity is None:
         return jsonify({"status": "error", "message": "Activity not found"})
     else:
-        #load images and convert them to base64
+        # load images and convert them to base64
         for act in activity["activities"]:
             images = []
             for image in act["images"]:
@@ -61,6 +63,7 @@ def post_activity_info():
     else:
         activities = []
     request.json["timestamp"] = datetime.now()
+    user_number = user_info.find({"_id": user_id})[0]["phone_number"]
 
     if activity_type == "user_session":
         state = request.json["state"]
@@ -74,24 +77,34 @@ def post_activity_info():
     elif activity_type == "doctor_notes":
         pass
     elif activity_type == "doctor_diagnosis":
-        #TODO: do something for the analytics on the map later
+        # TODO: do something for the analytics on the map later
         pass
     elif activity_type == "doctor_prescription":
-        #TODO: send a sms with the prescription
+        user_number = user_info.find({"_id": user_id})[0]["phone_number"]
+        send_sms(
+            user_number, "You have a new prescription: " + request.json["doctor_note"]
+        )
         pass
     elif activity_type == "live_meeting":
-        #TODO: send a sms and implement a live meeting using something
+        # TODO: send a sms and implement a live meeting using something
         pass
     elif activity_type == "more_info_request":
-        #TODO: send a sms asking for more info
+        user_number = user_info.find({"_id": user_id})[0]["phone_number"]
+        send_sms(
+            user_number,
+            "You have a new request from your doctor: " + request.json["doctor_note"],
+        )
         request.json["status"] = "pending"
     else:
         return jsonify({"status": "error", "message": "Invalid activity type"})
 
     activities.append(request.json)
-    activity_info.update_one({"_id": user_id}, {"$set": {"activities": activities}}, upsert=True)
+    activity_info.update_one(
+        {"_id": user_id}, {"$set": {"activities": activities}}, upsert=True
+    )
 
     return jsonify({"status": "success", "message": "Activity created"})
+
 
 @app.route("get_doctor_request", methods=["GET"])
 def get_doctor_request():
@@ -100,11 +113,16 @@ def get_doctor_request():
     if activity is None:
         return jsonify({"status": "error", "message": "Activity not found"})
     else:
-        #find if there is an activity type with status pending
+        # find if there is an activity type with status pending
         for act in activity["activities"]:
-            if act["activity_type"] == "more_info_request" and act["status"] == "pending":
+            if (
+                act["activity_type"] == "more_info_request"
+                and act["status"] == "pending"
+            ):
                 act["status"] = "completed"
-                activity_info.update_one({"_id": user_id}, {"$set": {"activities": activity["activities"]}})
+                activity_info.update_one(
+                    {"_id": user_id}, {"$set": {"activities": activity["activities"]}}
+                )
                 return jsonify(act)
     return jsonify({"status": "error", "message": "No pending request"})
 
