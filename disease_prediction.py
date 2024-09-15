964ce778a1effb7029e8c95d349a094f3fd173aa
dappon4 <dappon.04@gmail.com>
2024-09-14 23:03:53 -0400

    model implemented

--- disease_prediction.py
+++ disease_prediction.py
@@ -1,7 +1,34 @@
 import torch
 
-model = torch.load("./model.pth")
+model = torch.load("./model/model.pth")
 
 def predict(state):
-    #TODO: Implement this function
-    pass
+    """
+    state is a 17 element list containing the following information:
+    "body temperature in celcius": float,
+    "Respiratory rate": int,
+    "cough": bool,
+    "shortness of breath": bool,
+    "chest pain": bool,
+    "fatigue": bool,
+    "headache": bool,
+    "nausea": bool,
+    "body aches": bool,
+    "dizziness": bool,
+    "loss of taste": bool,
+    "loss of smell": bool,
+    "sore throat": bool,
+    "congestion": bool,
+    "runny nose": bool,
+    "diarrhea": bool,
+    "skin rash": bool,
+    
+    returns a list of 7 element Tensor
+    classes:
+    ["Covid-19", "Bronchitis", "Influenza", "Migraine", "Tuberculosis", "Meningitis", "Legionnaires' Disease"]
+    each representing the probability of the patient having the disease
+    """
+    
+    X = torch.tensor(state, dtype=torch.float32)
+    prediction = model(X)
+    return prediction
